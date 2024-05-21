2051ce59ec8e66722e27a41098eaf9147038963e
Lindfors Tarmo A <lindftar@cubbli22-gold.local>
2024-05-21 16:29:46 +0300

    initial front build

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import MyMap from './components/my-map';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+        <p>
+          Routing front.
+        </p>
+        
+        <MyMap />
+        
+
+    </div>
+  );
+}
+
+export default App;
