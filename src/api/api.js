7395a011e2cfc46a986f43da4dc83610b8ed7df6
pants64DS <pants64DS@users.noreply.github.com>
2024-05-31 15:56:40 +0300

    Read the address of the routing API from .env

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const ins = axios.create({
-  baseURL: 'https://routing-api-ohtuprojekti-staging.apps.ocp-test-0.k8s.it.helsinki.fi/'
+  baseURL: process.env.REACT_APP_ROUTING_API_URL || 'https://routing-api-ohtuprojekti-staging.apps.ocp-test-0.k8s.it.helsinki.fi/'
 });
 
 export default ins;
