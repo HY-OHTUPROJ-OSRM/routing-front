283faf448f43a033300085cd5c0dabc0f59b1a39
Lindfors Tarmo A <tarmo.lindfors@gmail.com>
2024-06-25 14:49:00 +0300

    add estimate of remaining time to progressbar alert

--- src/components/TimedAlert.js
+++ src/components/TimedAlert.js
@@ -4,18 +4,19 @@ import { Alert, ProgressBar } from 'react-bootstrap';
 import { removeTimedAlert } from '../features/messages/timedAlertSlice';
 import './TimedAlert.css';
 import { ROUTING_API_URL } from '../Utils/config';
-
+// A component that displays alerts for a certain amount of time. Displays also a progress bar on editmode saving, which is dynamically updated from backend url /status
 export default function TimedAlert() {
     const alerts = useSelector(state => state.timedAlert);
     const dispatch = useDispatch();
     const [percentage, setPercentage] = useState(0);
-
+    const [estimate, setEstimate] = useState(Date.now());
+    // Removes alerts after a certain amount of time
     useEffect(() => {
         alerts.forEach(alert => {
             setTimeout(() => dispatch(removeTimedAlert(alert.id)), alert.timeout);
         });
     }, [alerts, dispatch]);
-
+    // Fetches the progress from backend url /status
     useEffect(() => {
         
         
@@ -26,6 +27,8 @@ export default function TimedAlert() {
             const data = JSON.parse(event.data);
             if (data.status === 'processing') {
                 setPercentage(data.progress.percentage);
+                setEstimate(formatTime(Math.max((new Date(data.progress.estimate).getTime() - Date.now())/1000, 0)));
+                console.log(new Date(data.progress.estimate).getTime(), Date.now())
             }
         };
 
@@ -38,6 +41,17 @@ export default function TimedAlert() {
         };
     }, []);
 
+    const formatTime = (seconds) => {
+        if (seconds > 60) {
+            const minutes = Math.floor(seconds / 60);
+            const remainingSeconds = Math.floor(seconds % 60);
+            return `${minutes}m ${remainingSeconds}s`;
+        } else {
+            return `${Math.floor(seconds)} seconds`;
+        }
+    };
+
+
     return (
         <>
         {alerts.map(alert => (
@@ -45,6 +59,7 @@ export default function TimedAlert() {
                     <Alert variant={alert.variant} style={{width: "100%"}}>
                         {alert.text}
                         {alert.progress && <ProgressBar key={percentage} animated now={percentage} label={`${percentage}%`} />}
+                        {alert.progress && <p>Estimated time left: {estimate}</p>}
                     </Alert>
             </div>
         ))}
